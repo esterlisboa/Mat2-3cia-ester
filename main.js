be0b0d3866362d98d0785e7ddfe3a34f12df1f9a
esterlisboa <113607823+esterlisboa@users.noreply.github.com>
2024-04-04 17:21:27 -0300

    04/04

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -8,5 +9,8 @@ for(let i=0;i <botoes.length;i++){
         }
 
         botoes[i].classList.add("ativo");
+    
+    
     }
+    console.log();
 }
