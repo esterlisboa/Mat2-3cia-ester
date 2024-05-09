d1eee9e1616693dd6538966be9343d953179bb27
esterlisboa <113607823+esterlisboa@users.noreply.github.com>
2024-05-09 17:42:05 -0300

    09

--- main.js
+++ main.js
@@ -15,7 +15,14 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = tempoObjetivo1 - (tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual ;
+    let segundos = tempoFinal/1000;
+    return segundos ;
+}
