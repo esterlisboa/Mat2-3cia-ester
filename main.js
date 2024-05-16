6c9d9d475e7ebe4b1abfd03436d328444b05f8e2
esterlisboa <113607823+esterlisboa@users.noreply.github.com>
2024-05-16 17:16:52 -0300

    09

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
+for (let i = 0; i < botoes.length; i++) 
     botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
+        {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
@@ -23,6 +23,8 @@ contadores[0].textContent = tempoObjetivo1 - (tempoObjetivo1);
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date ();
     let tempoFinal = tempoObjetivo - tempoAtual ;
-    let segundos = tempoFinal/1000;
-    return segundos ;
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+
+    return minutos ;
 }
