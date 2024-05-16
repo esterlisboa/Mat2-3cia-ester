55bf7c78eee8caa2de9801b0d83f1dff8db9ba7b
esterlisboa <113607823+esterlisboa@users.noreply.github.com>
2024-05-16 17:38:57 -0300

    09

--- main.js
+++ main.js
@@ -27,4 +27,4 @@ function calculaTempo(tempoObjetivo){
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
-    return dias + horas ;
+    return dias + "dias" + horas ;
